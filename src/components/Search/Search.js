0e29a043d044c46738f760c43b696a6e640f504f
mark-nikerin <mrnikerin@gmail.com>
2020-06-27 15:32:27 +0400

    RWA-2 Add Main Title and Change Stateful Component to functional Component

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,25 +1,25 @@
-import React, { Component } from "react";
-import "../Search/search.css";
-import icons from "../../assets/search-icons.svg"
+import React from 'react';
+import '../Search/search.css';
+import icons from '../../assets/search-icons.svg'
 
-class Search extends Component {
-  render() {
+function Search() {
+
     return (
-      <div className="search">
-        <div className="search-field">
-          <svg className='search-icon' width="18" height="18">
+      <form className='search' >
+        <div className='search-field'>
+          <svg className='search-icon' width='18' height='18'>
             <use xlinkHref={`${icons}#search`}></use>
           </svg>
-          <input className="search-input" defaultValue="Samara"></input>
+          <input className='search-input' defaultValue='Samara'></input>
         </div>
-        <button className="search-button" type="submit">
-          <svg width="15" height="10">
+        <button className='search-button' type='submit'>
+          <svg width='15' height='10'>
             <use xlinkHref={`${icons}#arrow`}></use>
           </svg>
         </button>
-      </div>
+      </form>
     );
   }
-}
+
 
 export default Search;
--- src/components/WeatherApp.js
+++ src/components/WeatherApp.js
@@ -5,6 +5,7 @@ import '../index.css'
 function WeatherApp() {
   return (
     <div>
+      <h1 className='search-title'>Weather Forecast</h1>
       <Search className='search'/> 
     </div>
   );
