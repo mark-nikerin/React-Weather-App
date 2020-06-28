53b0ce987481d8ccf33fec1ea75c656c8ea4ab31
mark-nikerin <mrnikerin@gmail.com>
2020-06-29 00:07:18 +0400

    RWA-4 Add Forecast component`s files

---
+++ src/components/WeatherApp/Forecast/Forecast.js
@@ -0,0 +1,18 @@
+import React from 'react';  
+import ForecastItem from './ForecastItem';
+import './forecast.css';
+
+function Forecast(props) {
+
+
+  return (
+    <div className='forecast'>  
+    </div>
+  );
+}
+
+Forecast.propTypes = {
+
+}
+
+export default Forecast;
