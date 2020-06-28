dae7ede7e79da48769bc721bdb9c8abdf7db15b5
mark-nikerin <mrnikerin@gmail.com>
2020-06-28 22:26:46 +0400

    RWA-3 Format document

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -1,33 +1,39 @@
-import React  from 'react';
-import moment from 'moment';
-import PropTypes from 'prop-types';
-import '../Info/info.css';
-import icons from '../../assets/icons.svg';
+import React from "react";
+import moment from "moment";
+import PropTypes from "prop-types";
+import "../Info/info.css";
+import icons from "../../assets/icons.svg";
 
-
-function Info (props) {
-
+function Info(props) {
   const [currentInfo, locationName] = props;
 
-  let sign = currentInfo.TemperatureC > 0 ? "+" : currentInfo.TemperatureC === 0 ? "" : "-";
+  let sign =
+    currentInfo.TemperatureC > 0
+      ? "+"
+      : currentInfo.TemperatureC === 0
+      ? ""
+      : "-";
 
-  return(
-    <div className='info'>
-      <h1 className='info-location'>
-        <svg className='location-icon' width='18' height='26'>
+  return (
+    <div className="info">
+      <h1 className="info-location">
+        <svg className="location-icon" width="18" height="26">
           <use xlinkHref={`${icons}#location`}></use>
         </svg>
-        {locationName} {sign}{currentInfo.TemperatureC}°C
+        {locationName} {sign}
+        {currentInfo.TemperatureC}°C
       </h1>
-  <h2 className='info-date'> {moment(currentInfo.LocalObservationDateTime).format("dddd, MMMM D")}</h2>
+      <h2 className="info-date">
+        {" "}
+        {moment(currentInfo.LocalObservationDateTime).format("dddd, MMMM D")}
+      </h2>
     </div>
-  )
-  
+  );
 }
- 
+
 Info.propTypes = {
   currentInfo: PropTypes.object,
-  locationName: PropTypes.string
-}
+  locationName: PropTypes.string,
+};
 
 export default Info;
