249078a85eb18e54a824b3b74a0fae97fbfa8244
mark-nikerin <mrnikerin@gmail.com>
2020-06-30 03:36:11 +0400

    Updates

---
+++ precache-manifest.7a7ecb7daa6abdb07f3e60f13be43048.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbbb300922fd8d83cda73b05f7bf39bd",
+    "url": "/React-Weather-App/index.html"
+  },
+  {
+    "revision": "594a796e52b940f77402",
+    "url": "/React-Weather-App/static/css/4.ddf8b287.chunk.css"
+  },
+  {
+    "revision": "e26431527337bf3323cc",
+    "url": "/React-Weather-App/static/css/5.ceed0c16.chunk.css"
+  },
+  {
+    "revision": "8a97e5d85acd26a4482c",
+    "url": "/React-Weather-App/static/css/main.5bae98a6.chunk.css"
+  },
+  {
+    "revision": "b583ce442b9a12e4d105",
+    "url": "/React-Weather-App/static/js/0.7803e5ab.chunk.js"
+  },
+  {
+    "revision": "8f81858a0e0b71fba407",
+    "url": "/React-Weather-App/static/js/3.9c9cd4c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Weather-App/static/js/3.9c9cd4c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "594a796e52b940f77402",
+    "url": "/React-Weather-App/static/js/4.31ad43d2.chunk.js"
+  },
+  {
+    "revision": "e26431527337bf3323cc",
+    "url": "/React-Weather-App/static/js/5.8cf322ce.chunk.js"
+  },
+  {
+    "revision": "8a97e5d85acd26a4482c",
+    "url": "/React-Weather-App/static/js/main.4788067e.chunk.js"
+  },
+  {
+    "revision": "c20719a0b8a6021e3a97",
+    "url": "/React-Weather-App/static/js/runtime-main.52b3071e.js"
+  },
+  {
+    "revision": "9ed528d1c60daad92d9cb4006da72288",
+    "url": "/React-Weather-App/static/media/bg.9ed528d1.jpg"
+  },
+  {
+    "revision": "d3b0947f1be39f4b16a4a15c6719c707",
+    "url": "/React-Weather-App/static/media/icons.d3b0947f.svg"
+  },
+  {
+    "revision": "57333eb8aad381695fc8843c2fe94ae7",
+    "url": "/React-Weather-App/static/media/weather-icons.57333eb8.svg"
+  }
+]);
