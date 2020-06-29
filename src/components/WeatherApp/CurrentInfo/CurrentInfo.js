9689d06cb98f3d810367421d4e32417542691648
mark-nikerin <mrnikerin@gmail.com>
2020-06-29 23:10:51 +0400

    RWA-4 Add fetching 5-day forecast and rendering info

--- src/components/WeatherApp/CurrentInfo/CurrentInfo.js
+++ src/components/WeatherApp/CurrentInfo/CurrentInfo.js
@@ -3,14 +3,11 @@ import moment from "moment";
 import PropTypes from "prop-types";
 import "./currentInfo.css";
 import icons from "../../../assets/icons.svg";
+import weatherIcons from "../../../assets/weather-icons.svg";
 
 function CurrentInfo(props) {
-  let sign =
-    props.currentInfo.TemperatureC > 0
-      ? "+"
-      : props.currentInfo.TemperatureC === 0
-      ? ""
-      : "-";
+  const temp = props.currentInfo.Temperature.Metric;
+  let sign = temp.Value > 0 ? "+" : temp.Value === 0 ? "" : "-";
 
   return (
     <div className="info">
@@ -18,8 +15,12 @@ function CurrentInfo(props) {
         <svg className="location-icon" width="18" height="26">
           <use xlinkHref={`${icons}#location`}></use>
         </svg>
-        {props.locationName} {sign}
-        {props.currentInfo.TemperatureC}°C
+        {props.locationName}
+        <svg className="weather-icon" width="48" height="48">
+          <use xlinkHref={`${weatherIcons}#${props.currentInfo.WeatherIcon}`}></use>
+        </svg>
+        {sign}
+        {temp.Value}°{temp.Unit}
       </h1>
       <h2 className="info-date">
         {moment(props.currentInfo.LocalObservationDateTime).format("dddd, MMMM D")}
--- src/components/WeatherApp/WeatherApp.js
+++ src/components/WeatherApp/WeatherApp.js
@@ -13,6 +13,91 @@ function WeatherApp() {
 
   const [currentInfo, setCurrentInfo] = React.useState();
 
+  const [dailyForecast, setDailyForecast] = React.useState([]);
+
+  const getIcon = (id) => {
+    let iconId;
+  
+    switch (id) {
+      case 1:
+      case 2:
+      case 30:
+        iconId = "clear-day";
+        break;
+      case 3:
+      case 4:
+      case 5:
+        iconId = "clear-cloudy-day";
+        break;
+      case 6:
+        iconId = "partly-cloudy-day";
+        break;
+      case 7:
+      case 8:
+      case 11:
+        iconId = "cloudy";
+        break;
+      case 12:
+      case 18:
+        iconId = "showers";
+        break;
+      case 13:
+      case 14:
+        iconId = "drizzle-day";
+        break;
+      case 15:
+      case 16:
+      case 17:
+        iconId = "thunderstroms";
+        break;
+      case 19:
+      case 20:
+      case 21:
+      case 43:
+        iconId = "snow-flurries";
+        break;
+      case 22:
+      case 23:
+      case 24:
+      case 44:
+        iconId = "snow";
+        break;
+      case 25:
+      case 26:
+      case 29:
+        iconId = "sleet";
+        break;
+      case 31:
+      case 33:
+      case 34:
+        iconId = "clear-night";
+        break;
+      case 32:
+        iconId = "windy";
+        break;
+      case 35:
+      case 36:
+      case 37:
+        iconId = "clear-cloudy-night";
+        break;
+      case 38:
+        iconId = "partly-cloudy-night";
+        break;
+      case 39:
+      case 40:
+        iconId = "drizzle-night";
+        break;
+      case 41:
+      case 42:
+        iconId = "thunderstroms";
+        break;
+      default:
+        iconId = "clear-day";
+    }
+  
+    return iconId;
+  };
+  
   function getLocationInfo(locationQuery) {
     fetch(
       "http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=%20cxJJEtLXIeazgfGVT1Wod8r8bPLlUxtE&q=" +
@@ -33,8 +118,8 @@ function WeatherApp() {
         let locationInfo = result[0];
 
         if (locationInfo) {
-          setLocationInfo(result[0]);
-          getCurrentInfo(result[0].LocationKey);
+          setLocationInfo(locationInfo);
+          getCurrentInfo(locationInfo.LocationKey);
         }
       });
   }
@@ -50,15 +135,70 @@ function WeatherApp() {
         let result = conditions.map((x) => {
           let condition = {
             LocalObservationDateTime: x.LocalObservationDateTime,
-            WeatherIcon: x.WeatherIcon,
+            WeatherIcon: getIcon(x.WeatherIcon),
             IsDayTime: x.IsDayTime,
-            TemperatureC: x.Temperature.Metric.Value,
-            TemperatureF: x.Temperature.Imperial.Value,
+            Temperature: {
+              Imperial: {
+                Value: x.Temperature.Imperial.Value,
+                Unit: x.Temperature.Imperial.Unit,
+              },
+              Metric: {
+                Value: x.Temperature.Metric.Value,
+                Unit: x.Temperature.Metric.Unit,
+              }
+            }, 
           };
           return condition;
         });
- 
+        
         setCurrentInfo(result[0]);
+        getDailyForecast(locationKey);
+      });
+  }
+
+  function getDailyForecast(locationKey) {
+    fetch(
+      "http://dataservice.accuweather.com/forecasts/v1/daily/5day/" +
+        locationKey +
+        "?apikey=cxJJEtLXIeazgfGVT1Wod8r8bPLlUxtE&language=en-US&details=true&metric=true"
+    )
+      .then((response) => response.json())
+      .then((forecast) => {
+        let result = forecast.DailyForecasts.map((x) => {
+          let dayForecast = {
+            Date: x.Date ,
+            Day: {
+              Icon: getIcon(x.Day.Icon), 
+              Phrase: x.Day.ShortPhrase,
+              Temperature: {
+                Value: x.Temperature.Maximum.Value,
+                Unit: x.Temperature.Maximum.Unit,
+              },
+              Wind: {
+                SpeedValue: x.Day.Wind.Speed.Value,
+                SpeedUnit: x.Day.Wind.Speed.Unit,
+                DirectionDegrees: x.Day.Wind.Direction.Degrees,
+                DirectionName: x.Day.Wind.Direction.Localized
+              } 
+            } ,
+            Night: {
+              Icon: getIcon(x.Night.Icon), 
+              Phrase: x.Night.ShortPhrase,
+              Temperature: {
+                Value: x.Temperature.Minimum.Value,
+                Unit: x.Temperature.Minimum.Unit,
+              },
+              Wind: {
+                SpeedValue: x.Night.Wind.Speed.Value,
+                SpeedUnit: x.Night.Wind.Speed.Unit,
+                DirectionDegrees: x.Night.Wind.Direction.Degrees,
+                DirectionName: x.Night.Wind.Direction.Localized
+              } 
+            }
+          }; 
+          return dayForecast;
+        }); 
+        setDailyForecast(result);
       });
   }
 
@@ -88,10 +228,10 @@ function WeatherApp() {
           <CurrentInfo
             currentInfo={currentInfo}
             locationName={locationInfo.LocationName}
-          ></CurrentInfo>
+          />
+          <Forecast forecasts={dailyForecast}/>
         </div>
       )}
-      <Forecast></Forecast>
     </div>
   );
 }
